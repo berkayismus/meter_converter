130a49ebeb0dd86b60e0f4a6523b5b90cc304f89
Berkay ÇAYLI <berkay.cayli@defacto.com>
2023-10-21 17:06:53 +0300

    add basic functions on content view

--- MeterConverter/ContentView.swift
+++ MeterConverter/ContentView.swift
@@ -2,20 +2,95 @@
 //  ContentView.swift
 //  MeterConverter
 //
-//  Created by Berkay Çaylı on 21.10.2023.
+//  Created by Berkay Çaylı on 13.10.2023.
 //
 
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var input = 0.0
+    
+    let units = ["meter","kilometer","mil"]
+    @State private var selectedInputUnit = ""
+    
+    @State private var selectedOutputUnit = ""
+    @State private var result = 0.0
+    
+    @FocusState private var isFocused: Bool
+    
+  
+
+    
+    var inputValueInMeter : Double {
+        if selectedInputUnit == "kilometer" {
+           return input * 1000
+        } else if selectedInputUnit == "mil" {
+            return (input * 1.60) * 1000
+        } else {
+            return input
         }
-        .padding()
+        
+    }
+    
+    var ouputValue : Double {
+        if selectedOutputUnit == "kilometer" {
+            return inputValueInMeter / 1000
+        } else if selectedOutputUnit == "mil" {
+            return inputValueInMeter / 1000 / 1.60
+        } else {
+            return inputValueInMeter
+        }
+    }
+    
+    
+    
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section("Input") {
+                    TextField("Enter the value", value: $input, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($isFocused)
+                    
+                    Picker("Select Input Unit",selection: $selectedInputUnit) {
+                        ForEach(units,id: \.self) {
+                            Text($0)
+                        }
+                    }.pickerStyle(.navigationLink)
+                    
+                    
+                }
+                
+                Section("Output") {
+                    Picker("Select Output Unit",selection: $selectedOutputUnit) {
+                        ForEach(units,id: \.self) {
+                            Text($0)
+                        }
+                    }.pickerStyle(.segmented)
+                    
+                    
+             
+                    Section("Result") {
+                        Text(ouputValue, format: .number)
+
+                    }
+                }
+                
+            }.navigationTitle("MeterConverter")
+                .toolbar {
+                    if isFocused {
+                        Button("Done") {
+                            isFocused = false
+                        }
+                    }
+                }
+               
+        }
+        
+  
+        
+   
     }
 }
 
