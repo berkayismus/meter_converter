8ec4deeb3e9993e0af9944d43840db3ad85e2f3f
Berkay ÇAYLI <berkay.cayli@defacto.com>
2023-10-21 17:05:57 +0300

    Initial Commit

---
+++ MeterConverter/MeterConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeterConverterApp.swift
+//  MeterConverter
+//
+//  Created by Berkay Çaylı on 21.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct MeterConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
